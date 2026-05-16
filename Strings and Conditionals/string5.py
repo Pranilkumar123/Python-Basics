7831de6a961ba00fcb689832025bfd72a6618b80
Pranilkumar Pradip Salunkhe <pranilsalunkhe2007@gmail.com>
2026-05-16 09:04:41 +0530

    Add files via upload

---
+++ Strings and Conditionals/string5.py
@@ -0,0 +1,7 @@
+# 4. WAP to check if a number is a multiple of 7 or not.
+
+a = int(input("Enter a number :"))
+if(a%7 == 0):
+    print("It is multiple of 7")
+else:
+    print("Not a multiple of 7")
